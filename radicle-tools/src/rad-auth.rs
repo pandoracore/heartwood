80822f2765bffa3b697ff433f22402ae2b2dfd11
Slack Coder <slackcoder@server.ky>
2022-10-19 14:55:56 -0500

    rad-auth: allow running multiple times
    Instead of failing over when a key has been created, run as normal
    using the previously created profile.
    
    Signed-off-by: Slack Coder <slackcoder@server.ky>

--- radicle-tools/src/rad-auth.rs
+++ radicle-tools/src/rad-auth.rs
@@ -1,9 +1,11 @@
 fn main() -> anyhow::Result<()> {
-    let keypair = radicle::crypto::KeyPair::generate();
-    radicle::ssh::agent::register(&keypair.sk)?;
-
-    let profile = radicle::Profile::init(keypair)?;
-
+    let profile = if let Ok(v) = radicle::Profile::load() {
+        v
+    } else {
+        let keypair = radicle::crypto::KeyPair::generate();
+        radicle::ssh::agent::register(&keypair.sk)?;
+        radicle::Profile::init(keypair)?
+    };
     println!("id: {}", profile.id());
     println!("home: {}", profile.home.display());
 
