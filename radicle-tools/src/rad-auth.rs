e175f6ea4f80d6ac383ab036a0e72d3c3e910376
Slack Coder <slackcoder@server.ky>
2022-10-21 08:23:34 -0500

    rad-auth: create profile when not found
    Be careful about when to attempt creating a profile.  Errors could
    trigger creating a profile even when unrelated to it not existing.
    
    Profile::load(..) is altered to distinguish between these cases.
    
    Signed-off-by: Slack Coder <slackcoder@server.ky>

--- radicle-tools/src/rad-auth.rs
+++ radicle-tools/src/rad-auth.rs
@@ -1,10 +1,15 @@
+use radicle::profile;
+use radicle::Profile;
+
 fn main() -> anyhow::Result<()> {
-    let profile = if let Ok(v) = radicle::Profile::load() {
-        v
-    } else {
-        let keypair = radicle::crypto::KeyPair::generate();
-        radicle::ssh::agent::register(&keypair.sk)?;
-        radicle::Profile::init(keypair)?
+    let profile = match Profile::load() {
+        Ok(v) => v,
+        Err(profile::Error::NotFound(_)) => {
+            let keypair = radicle::crypto::KeyPair::generate();
+            radicle::ssh::agent::register(&keypair.sk)?;
+            radicle::Profile::init(keypair)?
+        }
+        Err(err) => anyhow::bail!(err),
     };
     println!("id: {}", profile.id());
     println!("home: {}", profile.home.display());
